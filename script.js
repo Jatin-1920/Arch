6df785e8e2e349b4092b6910deb97e661cc45b91
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-15 18:54:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 gsap.registerPlugin(ScrollTrigger)
-let scroll = document.querySelector(".root")
+let scroll = document.querySelector(".child")
 
 let x = scroll.offsetHeight
 gsap.to(".child",{
   y:-x,
   scrollTrigger:{
     trigger:".root",
-    pin:scroll,
+    pin:true,
     scrub:true,
     start:"top 20%",
     end:"max"
