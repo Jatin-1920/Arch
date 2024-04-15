b09cdf1cae88828aa5a9d7d5a553bd491a3a05fa
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-15 14:43:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const open = document.querySelector('#open')
-const close = document.querySelector('#close')
-const nav = document.querySelector('.nav')
-open.addEventListener("click",function(){
-nav.style.transform = "translateX(0%)"
-open.style.display="none"
-close.style.display= "block"
 
-})
-
-close.addEventListener("click",function() {
-    nav.style.transform="translateX(100%)"
-    close.style.display = "none"
-open.style.display="block"
-})
