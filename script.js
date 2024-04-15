9247e5d0a4839637ca7f73807695ddb26e5425d9
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-15 17:06:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+gsap.registerPlugin(ScrollTrigger)
+const scroll = document.querySelector(".root")
 
+let x = scroll.scrollHeight
+gsap.to(scroll,{
+  scrollTrigger:{
+    trigger:".root",
+    pin:true,
+    scrub:true,
+    start:"top 20%",
+    end:"bottom bottom"
+  }
+})
