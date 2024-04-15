1352133d22fec97b2e0200f816263f443679d6ad
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-15 19:28:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 gsap.registerPlugin(ScrollTrigger)
-let child = document.querySelector(".child")
 
-const tween = gsap.to(child,{
-  onUpdate:()=>
-      tween.scrollTrigger.spacer.style.height
-      = child.offsetHeight+"px",
-  scrollTrigger:{
-    padding:40,
-    trigger:".container",
-    pin:true,
-    scrub:true,
-    start:"top top",
-    end:"max",
-    pinSpacing:false
-  }})
+
+let container = document.querySelector(".container")
+    let child = document.querySelector(".child")
+    tween = gsap.to(child, {
+      padding: 40,
+      onUpdate: () => tween.scrollTrigger.spacer.style.height = child.offsetHeight + "px",
+      scrollTrigger: {
+        trigger: container,
+        start: 'top top',
+        end: "max",
+        pin: true,
+        pinSpacing: false,
+        toggleActions: "play none none reverse",
+      }
+    });
 
   
 
