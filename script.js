f822ebd5fe76edf6f3a2a93ed225e285998c6bbc
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-16 09:34:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ trigger.forEach(e=>{
 })
   
 
-let photos = gsap.utils.toArray(".imgDiv:not(:first-child)")
+let photos = gsap.utils.toArray(".imgDiv:not(:last-child)")
 const allPhotos = gsap.utils.toArray(".imgDiv")
 const content = gsap.utils.toArray(".pinh1:not(:first-child)")
 gsap.set(photos,{
-clipPath:"inset(100% 0 0 0)"
+clipPath:"inset(0px 0px 0% 0px)"
 })
 
 ScrollTrigger.create({
@@ -32,7 +32,7 @@ ScrollTrigger.create({
     })
 content.forEach((e,index)=>{
     const animation = gsap.timeline().to(photos[index],{
-        clipPath:"inset(0% 0 0 0)"
+        clipPath:"inset(0px 0px 100% 0px)"
         
     })
     ScrollTrigger.create({
