cffce62eb3309f6730a54723cc8aa4f8b8a52072
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-16 08:38:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,12 +26,11 @@ gsap.set(photos,{
 
 ScrollTrigger.create({
         trigger:".pinSection",
-        start:"top 30%",
+        start:"top 20%",
         pin:".pinImg"
     })
 content.forEach((e,index)=>{
-    const animation = gsap.timline()
-    animation.to(photos[index],{
+    const animation = gsap.timline().to(photos[index],{
         clipPath:"inset(0px 0px 100% 0px)"
     })
     ScrollTrigger.create({
