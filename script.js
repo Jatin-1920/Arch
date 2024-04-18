6b82a03beb095946650f8265ff383ccdb4a5ddd8
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-18 08:28:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const animTl = gsap.timeline()
 animTl.to(".toplayer",{
   
  duration:1.6,
-  ease:"expo.inOut",
+  ease:"power2.inOut",
   stagger:{
     amount:0.4
   },
@@ -32,7 +32,7 @@ animTl.to(".toplayer",{
 }).to(".layer",{
   delay:-0.8,
  duration:1.6,
-  ease:"expo.inOut",
+  ease:"power2.inOut",
   xPercent:100,
     stagger:{
     amount:0.4
@@ -44,13 +44,13 @@ animTl.to(".toplayer",{
   stagger:{
     amount:0.4
   },
-  ease:"expo.inOut",
+  ease:"power2.inOut",
 })
 gsap.to(".layerHead h1",{
   delay:2.8,
  display:"flex",
   duration:.5,
-  ease:"expo.inOut",
+  ease:"power2.inOut",
 })
 
 const movie = document.querySelectorAll(".movieBtn")
