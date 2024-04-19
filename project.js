60dde76bf4bcafecdd5d8dfa5a8b8ff646ea80a6
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-19 19:12:24 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,1 +1,11 @@
 
+document.addEventListener("mousemove",(e)=>{
+
+  const x = e.clientX
+  const y = e.clientY
+  const centerX = window.innerWidth/2
+  const centerY = window.innerHeight/2
+
+  const precentX = (x-centerX) / centerX
+  const precentY = (y-centerY) / centerY
+})
