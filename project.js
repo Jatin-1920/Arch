42125e49785aeb7edf60f2fe4ecf86212cbe24b4
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-19 19:14:40 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,5 +1,5 @@
-
-document.addEventListener("mousemove",(e)=>{
+const archCon = select(".archContainer")
+archCon.addEventListener("mousemove",(e)=>{
 
   const x = e.clientX
   const y = e.clientY
@@ -8,4 +8,7 @@ document.addEventListener("mousemove",(e)=>{
 
   const precentX = (x-centerX) / centerX
   const precentY = (y-centerY) / centerY
+
+  const rotateX = 55 + percentY *2
+  const rotateY =  percentX *2
 })
