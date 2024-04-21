bf5932a3fc26978fee479b9b114b75b532a932e0
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-21 14:03:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger,Flip)
 
 
-/*const lenis = new Lenis({
+const lenis = new Lenis({
   smooth:true,
   smoothTouch:false,
   lerp:0.1,
@@ -17,42 +17,6 @@ gsap.ticker.add((time)=>{
 })
 
 gsap.ticker.lagSmoothing(0) 
-
-*/
-
-
-
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector('.root'),
-  smooth: true,
-  lerp:0.05,
-  
-});
-
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-
-locoScroll.on('scroll', ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy('.root', {
-  scrollTop(value) {
-    return arguments.length
-      ? locoScroll.scrollTo(value, 0, 0)
-      : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {
-      top: 0,
-      left: 0,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector('.root').style.transform
-    ? 'transform'
-    : 'fixed',
-});
 
 
 
@@ -296,7 +260,7 @@ ScrollTrigger.create({
       parallaxDiv.forEach(e=>{
   
 
-   gsap.fromTo(parallaxImg,{
+   gsap.fromTo(e,{
     x:"-30vw",
 },{
   x:0,
@@ -312,8 +276,6 @@ ScrollTrigger.create({
 })
 })
 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-  ScrollTrigger.refresh(); 
+ 
 
   
