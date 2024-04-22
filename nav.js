2969c9c9a4eba30f755e3c29e522564ba250cd7b
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-22 09:31:55 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,1 +1,17 @@
+const lenis = new Lenis({
+  smooth:true,
+  smoothTouch:false,
+  lerp:0.1,
+  duration:1.5
+})
+
+
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0) 
 
