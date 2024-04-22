c821a62363c3deaa97e808051a0adea60aada179
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-22 11:36:29 +0530

    Update about.js

--- about.js
+++ about.js
@@ -1,1 +1,10 @@
 
+
+const vignetteAnim =[
+  {img:"arch1.jpg",vigImg:"arch4.jpg"},
+  {img:"arch78.jpg",vigImg:"arch36.jpg"},
+    {img:"arch109.jpg",vigImg:"arch123.jpg"},
+    {img:"archi2.jpeg",vigImg:"archi1.jpeg"},
+]
+
+vignetteAnim.map((item,index))
